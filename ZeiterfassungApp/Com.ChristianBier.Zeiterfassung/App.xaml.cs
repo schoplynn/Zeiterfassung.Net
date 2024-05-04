99dcb1688a0f893ea46e9ffaaa71239e610d02cc
Christian Bier <christian.bier@live.de>
2024-05-04 09:50:28 +0200

    - Versucht Multi Language Support hinzuzufügen

--- ZeiterfassungApp/Com.ChristianBier.Zeiterfassung/App.xaml.cs
+++ ZeiterfassungApp/Com.ChristianBier.Zeiterfassung/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,18 @@ namespace Com.ChristianBier.Zeiterfassung
     /// </summary>
     public partial class App : Application
     {
+        App()
+        {
+            CultureInfo culture = CultureInfo.CurrentCulture;
+
+            if (culture.TwoLetterISOLanguageName != "de")
+            {
+                culture = new CultureInfo("en");
+            }
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture.TwoLetterISOLanguageName);
+
+            // Zum Testen kann die Culture Info manuell gesetzt werden
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+        }
     }
 }
